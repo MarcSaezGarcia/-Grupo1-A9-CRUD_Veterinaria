8d54f946277e140900745eb13fb585e906b9d94b
Marc <468254.joan23@fje.edu>
2026-05-06 12:33:42 +0200

    Subir EXCEL y actuazliar BBDD

--- SQL/database.sql
+++ SQL/database.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
 -- VETERINARIOS
 CREATE TABLE IF NOT EXISTS veterinarios (
     id_veterinario INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
+    nombre VARCHAR(75) NOT NULL,
     telefono VARCHAR(15) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     especialidad VARCHAR(100) NOT NULL,
@@ -22,16 +22,16 @@ CREATE TABLE IF NOT EXISTS veterinarios (
 -- PROPIETARIOS
 CREATE TABLE IF NOT EXISTS propietarios (
     id_propietario INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
+    nombre VARCHAR(75) NOT NULL,
     telefono VARCHAR(15) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
-    dni VARCHAR(9) NOT NULL UNIQUE
+    DNI VARCHAR(9) NOT NULL UNIQUE
 );
 
 -- RAZAS
 CREATE TABLE IF NOT EXISTS razas (
     id_raza INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
+    nombre VARCHAR(75) NOT NULL,
     caracteristicas_fisicas TEXT NOT NULL,
     comportamiento TEXT NOT NULL
 );
@@ -40,10 +40,10 @@ CREATE TABLE IF NOT EXISTS razas (
 CREATE TABLE IF NOT EXISTS mascotas (
     id_mascota INT AUTO_INCREMENT PRIMARY KEY,
     chip VARCHAR(50) NOT NULL UNIQUE,
-    nombre VARCHAR(50) NOT NULL,
-    sexo ENUM('M','F') NOT NULL,
-    especie VARCHAR(50) NOT NULL,
-    fecha_nacimiento DATE NOT NULL,
+    nombre VARCHAR(75) NOT NULL,
+    sexo ENUM('Macho','Hembra') NOT NULL,
+    especie ENUM('Perro','Gato','Ave','Reptil','Otro') NOT NULL
+    fecha_nacimiento  NOT NULL,
     id_raza INT NOT NULL,
     id_propietario INT NOT NULL,
     id_veterinario INT NOT NULL,
